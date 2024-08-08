34d3d2372f9508b18f8da8c4da76dfcf2d43ac81
Martin Barri <maeertin@gmail.com>
2024-08-08 14:36:54 +0200

    update scripts, add new utils package

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from "react";
-import clsx from "clsx";
-import classes from "./button.module.css";
+import { ReactNode } from 'react'
+import clsx from 'clsx'
+import { normalize } from '@maeertin/utils'
+import classes from './button.module.css'
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+  children: ReactNode
+  className?: string
+  appName: string
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, className, appName } = props;
+  const { children, className, appName } = props
 
   return (
     <button
@@ -17,6 +18,7 @@ export const Button = (props: ButtonProps) => {
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
+      <p>normalize test: {normalize(30, 0, 100)}</p>
     </button>
-  );
-};
+  )
+}
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, type Options } from "tsup";
+import { defineConfig, type Options } from 'tsup'
 // import CssModulesPlugin from "esbuild-css-modules-plugin";
-import path from "path";
-import fsPromises from "fs/promises";
-import postcss from "postcss";
-import postcssModules from "postcss-modules";
-import { generateScopedName } from "hash-css-selector";
+import path from 'path'
+import fsPromises from 'fs/promises'
+import postcss from 'postcss'
+import postcssModules from 'postcss-modules'
+// @ts-expect-error -- TODO: Fix TS error
+import { generateScopedName } from 'hash-css-selector'
 
 export default defineConfig((options: Options) => ({
   // Build react package for use in nextjs 13
@@ -12,20 +13,20 @@ export default defineConfig((options: Options) => ({
   esbuildOptions(internalOptions) {
     internalOptions.banner = {
       js: '"use client"',
-    };
+    }
   },
   treeshake: false, // Enabling this removes client directives.
   splitting: false,
-  entry: ["src/**/*.{ts,tsx}"],
-  format: ["esm", "cjs"],
+  entry: ['src/**/*.{ts,tsx}'],
+  format: ['esm', 'cjs'],
   loader: {
-    ".css": "local-css",
+    '.css': 'local-css',
   },
   dts: true,
   sourcemap: true,
   minify: true,
   clean: true,
-  external: ["react"],
+  external: ['react'],
   // esbuildPlugins: [
   //   CssModulesPlugin({
   //     // @see https://github.com/indooorsman/esbuild-css-modules-plugin/blob/main/index.d.ts for more details
@@ -42,70 +43,54 @@ export default defineConfig((options: Options) => ({
   // https://github.com/egoist/tsup/issues/536
   esbuildPlugins: [
     {
-      name: "css-module",
+      name: 'css-module',
       setup(build): void {
-        build.onResolve(
-          { filter: /\.module\.css$/, namespace: "file" },
-          (args) => {
-            return {
-              path: `${args.path}#css-module`,
-              namespace: "css-module",
-              pluginData: {
-                pathDir: path.join(args.resolveDir, args.path),
+        build.onResolve({ filter: /\.module\.css$/, namespace: 'file' }, (args) => {
+          return {
+            path: `${args.path}#css-module`,
+            namespace: 'css-module',
+            pluginData: {
+              pathDir: path.join(args.resolveDir, args.path),
+            },
+          }
+        })
+        build.onLoad({ filter: /#css-module$/, namespace: 'css-module' }, async (args) => {
+          const { pluginData } = args as {
+            pluginData: { pathDir: string }
+          }
+
+          const source = await fsPromises.readFile(pluginData.pathDir, 'utf8')
+
+          let cssModule: any = {}
+          const result = await postcss([
+            postcssModules({
+              generateScopedName: function(name, filename) {
+                const newSelector = generateScopedName(name, filename)
+                cssModule[name] = newSelector
+
+                return newSelector
               },
-            };
-          },
-        );
-        build.onLoad(
-          { filter: /#css-module$/, namespace: "css-module" },
-          async (args) => {
-            const { pluginData } = args as {
-              pluginData: { pathDir: string };
-            };
+              getJSON: () => { },
+              scopeBehaviour: 'local',
+            }),
+          ]).process(source, { from: pluginData.pathDir })
 
-            const source = await fsPromises.readFile(
-              pluginData.pathDir,
-              "utf8",
-            );
-
-            let cssModule: any = {};
-            const result = await postcss([
-              postcssModules({
-                generateScopedName: function(name, filename) {
-                  const newSelector = generateScopedName(name, filename);
-                  cssModule[name] = newSelector;
-
-                  return newSelector;
-                },
-                getJSON: () => { },
-                scopeBehaviour: "local",
-              }),
-            ]).process(source, { from: pluginData.pathDir });
-
-            return {
-              pluginData: { css: result.css },
-              contents: `import "${pluginData.pathDir
-                }"; export default ${JSON.stringify(cssModule)}`,
-            };
-          },
-        );
-        build.onResolve(
-          { filter: /\.module\.css$/, namespace: "css-module" },
-          (args) => ({
-            path: path.join(args.resolveDir, args.path, "#css-module-data"),
-            namespace: "css-module",
-            pluginData: args.pluginData as { css: string },
-          }),
-        );
-        build.onLoad(
-          { filter: /#css-module-data$/, namespace: "css-module" },
-          (args) => ({
-            contents: (args.pluginData as { css: string }).css,
-            loader: "css",
-          }),
-        );
+          return {
+            pluginData: { css: result.css },
+            contents: `import "${pluginData.pathDir}"; export default ${JSON.stringify(cssModule)}`,
+          }
+        })
+        build.onResolve({ filter: /\.module\.css$/, namespace: 'css-module' }, (args) => ({
+          path: path.join(args.resolveDir, args.path, '#css-module-data'),
+          namespace: 'css-module',
+          pluginData: args.pluginData as { css: string },
+        }))
+        build.onLoad({ filter: /#css-module-data$/, namespace: 'css-module' }, (args) => ({
+          contents: (args.pluginData as { css: string }).css,
+          loader: 'css',
+        }))
       },
     },
   ],
   ...options,
-}));
+}))
